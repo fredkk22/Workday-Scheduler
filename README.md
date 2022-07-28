430aefabbefc0342b8210fb44f304d75fc5d4f39
fredkk22 <freddykwak123@gmail.com>
2022-07-27 23:15:13 -0400

    Edited script.js file and README.md file
    Completed the application, and added to the README.md file. Still have to add more comments and clean up the code a bit!

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Workday-Scheduler
+
+## Table of Contents
+  - [Description](#description)
+  - [Screenshots](#screenshots)
+  - [Credits](#credits)
+    - [Contributors](#contributors)
+    - [Links Used for Reference:](#links-used-for-reference)
+  - [Link to Deployed Application](#link-to-deployed-application)
+
+## Description
+
+This Module 5 Challenge was to create a workday scheduler for the user to be able to record any meetings, dues, or plans through the workday and keep their hourly schedule stored on the application even after refreshing the page.
+
+The reason for creating a random password generator was to practice coding in jQuery, moment.js, and bootstrap for use in practical applications.
+
+I was able to learn everything I listed above and have become much more comfortable using jQuery, moment.js, and Bootstrap. I still have yet to gain more experience in all of them, but I feel the most confident in jQuery and the least confident in Bootstrap as of right now.
+
+## Video Recording
+
+[![Watch Video!](https://img.youtube.com/vi/xP8-WbizifY/maxresdefault.jpg)](https://youtu.be/xP8-WbizifY)
+
+## Credits
+
+### Contributors
+
+Sean Dillon https://github.com/DeviousDoge/
+
+Juliet Lamond https://github.com/jlamond37
+
+Natham Soum https://github.com/natesoum
+
+Joshua Rae https://github.com/joshuar2155
+
+### Links Used for Reference:
+
+https://momentjs.com/docs/#/displaying/format/
+
+https://jqueryui.com/widget/
+
+https://getbootstrap.com/docs/4.3/getting-started/introduction/
+
+https://stackoverflow.com/questions/1414365/disable-enable-an-input-with-jquery
+
+## Link to Deployed Application
+
+https://fredkk22.github.io/Workday-Scheduler/
--- script.js
+++ script.js
@@ -1,13 +1,5 @@
+// Declare Variables
 var currentDay = $("#currentDay");
-var input1 = $("#input1");
-var input2 = $("#input2");
-var input3 = $("#input3");
-var input4 = $("#input4");
-var input5 = $("#input5");
-var input6 = $("#input6");
-var input7 = $("#input7");
-var input8 = $("#input8");
-var input9 = $("#input9");
 var hour1 = $(".hour1").text("9:00 AM");
 var hour2 = $(".hour2").text("10:00 AM");
 var hour3 = $(".hour3").text("11:00 AM");
@@ -20,122 +12,109 @@ var hour9 = $(".hour9").text("5:00 PM");
 var past = $(".past");
 var present = $(".present");
 var future = $(".future");
-var allInputs = [input1, input2, input3, input4, input5, input6, input7, input8, input9];
-var allHours = [hour1.text(), hour2.text(), hour3.text(), hour4.text(), hour5.text(), hour6.text(), hour7.text(), hour8.text(), hour9.text()];
 var everyInput = $(".allinputs");
-
+var allInputs = [$("#input1"), $("#input2"), $("#input3"), $("#input4"), $("#input5"), $("#input6"), $("#input7"), $("#input8"), $("#input9")];
+var allHours = [hour1.text(), hour2.text(), hour3.text(), hour4.text(), hour5.text(), hour6.text(), hour7.text(), hour8.text(), hour9.text()];
+// Display current date
 currentDay.text(moment().format("dddd, MMMM Do"))
-
+// Initially enable all the time blocks
 everyInput.prop("disabled", false);
-
-function inputToggle1() {
-    if (input1.prop("disabled")) {
-        input1.prop("disabled", false);
-    } else if (!input1.prop("disabled")) {
-        input1.prop("disabled", true);
-    }
-}
-function inputToggle2() {
-    if (input2.prop("disabled")) {
-        input2.prop("disabled", false);
-    } else if (!input2.prop("disabled")) {
-        input2.prop("disabled", true);
-    }
-}
-function inputToggle3() {
-    if (input3.prop("disabled")) {
-        input3.prop("disabled", false);
-    } else if (!input3.prop("disabled")) {
-        input3.prop("disabled", true);
-    }
-}
-function inputToggle4() {
-    if (input4.prop("disabled")) {
-        input4.prop("disabled", false);
-    } else if (!input4.prop("disabled")) {
-        input4.prop("disabled", true);
-    }
-
-}
-function inputToggle5() {
-    if (input5.prop("disabled")) {
-        input5.prop("disabled", false);
-    } else if (!input5.prop("disabled")) {
-        input5.prop("disabled", true);
-    }
-
-}
-function inputToggle6() {
-    if (input6.prop("disabled")) {
-        input6.prop("disabled", false);
-    } else if (!input6.prop("disabled")) {
-        input6.prop("disabled", true);
-    }
-
-}
-function inputToggle7() {
-    if (input7.prop("disabled")) {
-        input7.prop("disabled", false);
-    } else if (!input7.prop("disabled")) {
-        input7.prop("disabled", true);
-    }
-
-}
-function inputToggle8() {
-    if (input8.prop("disabled")) {
-        input8.prop("disabled", false);
-    } else if (!input8.prop("disabled")) {
-        input8.prop("disabled", true);
-    }
-
-}
-function inputToggle9() {
-    if (input9.prop("disabled")) {
-        input9.prop("disabled", false);
-    } else if (!input9.prop("disabled")) {
-        input9.prop("disabled", true);
-    }
-
-}
-
+// Clicking one of the buttons next to the schedule saves the scheduled plan for that specific row
 $(".container").on("click", function (event) {
     event.preventDefault();
     var element = event.target;
 
     if (element.matches(".disableinput1")) {
-        inputToggle1();
+        var sched1 = [];
+        allInputs[0].val();
+        sched1.push(allInputs[0].val());
+        localStorage.setItem("9AM", JSON.stringify(sched1));
     } else if (element.matches(".disableinput2")) {
-        inputToggle2();
+        var sched2 = [];
+        allInputs[1].val();
+        sched2.push(allInputs[1].val());
+        localStorage.setItem("10AM", JSON.stringify(sched2));
     } else if (element.matches(".disableinput3")) {
-        inputToggle3();
+        var sched3 = [];
+        allInputs[2].val();
+        sched3.push(allInputs[2].val());
+        localStorage.setItem("11AM", JSON.stringify(sched3));
     } else if (element.matches(".disableinput4")) {
-        inputToggle4();
+        var sched4 = [];
+        allInputs[3].val();
+        sched4.push(allInputs[3].val());
+        localStorage.setItem("12PM", JSON.stringify(sched4));
     } else if (element.matches(".disableinput5")) {
-        inputToggle5();
+        var sched5 = [];
+        allInputs[4].val();
+        sched5.push(allInputs[4].val());
+        localStorage.setItem("1PM", JSON.stringify(sched5));
     } else if (element.matches(".disableinput6")) {
-        inputToggle6();
+        var sched6 = [];
+        allInputs[5].val();
+        sched6.push(allInputs[5].val());
+        localStorage.setItem("2PM", JSON.stringify(sched6));
     } else if (element.matches(".disableinput7")) {
-        inputToggle7();
+        var sched7 = [];
+        allInputs[6].val();
+        sched7.push(allInputs[6].val());
+        localStorage.setItem("3PM", JSON.stringify(sched7));
     } else if (element.matches(".disableinput8")) {
-        inputToggle8();
+        var sched8 = [];
+        allInputs[7].val();
+        sched8.push(allInputs[7].val());
+        localStorage.setItem("4PM", JSON.stringify(sched8));
     } else if (element.matches(".disableinput9")) {
-        inputToggle9();
+        var sched9 = [];
+        allInputs[8].val();
+        sched9.push(allInputs[8].val());
+        localStorage.setItem("5PM", JSON.stringify(sched9));
     }
 });
-
+// Function for coloring in an disabling blocks based on current time
+function allHourBlocks() {
 for (var i = 0; i < allHours.length; i++) {
     if (moment(allHours[i], "LT").format("HH") < moment().format("HH")) {
         allInputs[i].removeClass("future");
         allInputs[i].removeClass("present");
+        allInputs[i].prop("disabled", true);
     } else if (moment(allHours[i], "LT").format("HH") === moment().format("HH")) {
         allInputs[i].removeClass("future");
         allInputs[i].removeClass("past");
+        allInputs[i].prop("disabled", false);
     } else if (moment(allHours[i], "LT").format("HH") > moment().format("HH")) {
         allInputs[i].removeClass("past");
         allInputs[i].removeClass("present");
+        allInputs[i].prop("disabled", false);
     }
 
     if (moment().format("HH") > 17 || moment().format("HH") < 9) {
         everyInput.addClass("future");
+        allInputs[i].prop("disabled", false);
     }
+}
 };
+// Function for storing and rendering the schedule
+function renderSched() {
+    var storedSched1 = JSON.parse(localStorage.getItem("9AM"));
+    allInputs[0].val(storedSched1);
+    var storedSched2 = JSON.parse(localStorage.getItem("10AM"));
+    allInputs[1].val(storedSched2);
+    var storedSched3 = JSON.parse(localStorage.getItem("11AM"));
+    allInputs[2].val(storedSched3);
+    var storedSched4 = JSON.parse(localStorage.getItem("12PM"));
+    allInputs[3].val(storedSched4);
+    var storedSched5 = JSON.parse(localStorage.getItem("1PM"));
+    allInputs[4].val(storedSched5);
+    var storedSched6 = JSON.parse(localStorage.getItem("2PM"));
+    allInputs[5].val(storedSched6);
+    var storedSched7 = JSON.parse(localStorage.getItem("3PM"));
+    allInputs[6].val(storedSched7);
+    var storedSched8 = JSON.parse(localStorage.getItem("4PM"));
+    allInputs[7].val(storedSched8);
+    var storedSched9 = JSON.parse(localStorage.getItem("5PM"));
+    allInputs[8].val(storedSched9);
+}
+
+renderSched();
+allHourBlocks();
